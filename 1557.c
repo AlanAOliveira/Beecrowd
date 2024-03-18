6e3e368e0b9ac7815167d44e7f86ad0cc8ba7968
AlanAOliveira <alanalves123456789@gmail.com>
2024-03-18 11:10:57 -0300

    Update 1557.c

--- 1557.c
+++ 1557.c
@@ -3,10 +3,9 @@
  
 int main() {
  
-    int Ordem, i, j;
+    int Ordem, i, j, espacos[15] = {1,1,2,2,3,4,4,5,5,6,7,7,8,8,9};
     
     scanf("%i", &Ordem);
-    
     while(Ordem){
         
         int matriz[Ordem][Ordem];
@@ -15,8 +14,11 @@ int main() {
                 matriz[i][j] = pow(2,i+j);
                 
         for(i=0; i< Ordem;i++){
-            for(j=0; j< Ordem;j++)
-                printf("%i",matriz[i][j]);
+            for(j=0; j< Ordem;j++){
+                printf("%*i",espacos[Ordem-1],matriz[i][j]);
+                if(j != (Ordem - 1))
+                    printf(" ");
+            }
             printf("\n");
         }
         scanf("%i", &Ordem);
